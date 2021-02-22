8be6b8984a272924263bc51a52cacac8103a836d
Alexander Naiden <naidenav@gmail.com>
2021-02-22 20:24:35 +0300

    init: implement task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    while (true) {
+        let strLength = str.length;
+        bracketsConfig.forEach(bracketPair => {
+            str = str.replace(bracketPair[0] + bracketPair[1], '')
+        });
+
+        if (strLength === str.length) {
+            break;
+        }
+    }
+
+    return str.length === 0;
 }
+
+    
